d8d4875d539e0ed7804812b187c6c60218f36b9d
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2021-06-02 15:19:55 -0700

    JS Navbar Test
    WHY WONT IT WORK

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+console.log('hello world');
+
+document.addEventListener('scroll', () => {
+  console.log('helloworld2');
+});
+
+document.addEventListener('click', () => {
+    console.log('helloworld3');
+  });
