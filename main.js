de08f1e728052b837b6f512055689d8c8bbb171d
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2021-06-03 14:30:51 -0700

    JS Navbar
    I hate this

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 'use strict';
 
-console.log('hello world');
-
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  console.log('WORKAJSDKLASJDL');
+  console.log(window.scrollY); 
+  console.log(navbarHeight);
+if (window.scrollY > navbarHeight){
+  navbar.classList.add('navbar--dark');
+} else {
+  navbar.classList.remove('navbar--dark');
+}
 });
