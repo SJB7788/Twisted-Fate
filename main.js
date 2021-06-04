2cd3360fc068b9c5b0396875c5c6b7d058d697a4
SJB7788 <83483363+SJB7788@users.noreply.github.com>
2021-06-04 14:10:53 -0700

    JS Title

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 'use strict';
 
+//navbar transparent when its not scrolled down
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  console.log(window.scrollY); 
-  console.log(navbarHeight);
 if (window.scrollY > navbarHeight){
   navbar.classList.add('navbar--dark');
 } else {
   navbar.classList.remove('navbar--dark');
 }
 });
+
+//make twisted fate text disappear as you scroll
+const home = document.querySelector('.title-title');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () =>{
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+})
